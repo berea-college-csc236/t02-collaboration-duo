42cf6b0c6a5bd54ab122285d0b596bd8f794a44c
glek <156453486+Kafui123@users.noreply.github.com>
2024-08-26 10:56:11 -0400

    Did student 1 portion of work

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-    Kafui Gle -- to be added by student 1
-    Finagnon Adjagbodjou -- to be added by student 2
+    Kafui Gle
+  
     TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
  */
 #include <iostream>
